fed0a2f8787c31d941002b00ed2aeba2a5e0d324
Felipe Anibal <felipeanibalnunesbrito@gmail.com>
2024-09-10 16:15:40 -0300

    core: add material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import './App.css'
+import Button from '@mui/material/Button';
+
 
 function App() {
-
+  
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
-    </div>
+    <Button variant="contained">Hello world</Button>
   )
 }
 

20c180dac71ae5c70aa77f07513a6039c0a3bde4
Felipe Anibal <felipeanibalnunesbrito@gmail.com>
2024-09-26 20:09:20 -0300

    build: Add react-router-dom to dependencies
    React Router will be used to provide routing and linking between pages.
    
    Signed-off-by: Felipe Anibal <felipeanibalnunesbrito@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 
 function App() {
-
   return (
-    <HomePage></HomePage>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
   );
 }
 

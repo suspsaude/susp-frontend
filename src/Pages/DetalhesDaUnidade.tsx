477fc6d65b7de7792dba614e3760990e53e2fc2f
Gui Wallace <guiwallace@usp.br>
2024-11-07 16:57:43 -0300

    feat - Details page fetches health unit data and show this data dynamically

--- src/Pages/DetalhesDaUnidade.tsx
+++ src/Pages/DetalhesDaUnidade.tsx
@@ -10,6 +10,8 @@ import StarIcon from '@mui/icons-material/Star';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import PeopleIcon from '@mui/icons-material/People';
 import InfoCard from '../Components/InfoCard';
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
 const ListaDeUnidades: React.FC = () => {
 
@@ -17,25 +19,64 @@ const ListaDeUnidades: React.FC = () => {
     const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
     const isMediumScreen = useMediaQuery(theme.breakpoints.down('md'));
 
+    const location = useLocation();
+    const {cnesNumber} = location.state || {};
+
+    const [facilityDetails, setFacilityDetais] =  useState<{ name: string }>({ name: '' });
+    
+    const fetchFacitilyDetails = async () => {
+        try {
+            const response = await fetch(`http://0.0.0.0:8000/unidades/detalhes?cnes=${cnesNumber}`);
+
+            if (!response.ok) {
+                throw new Error("Erro ao buscar unidades de saúde");
+            }
+
+            const data = await response.json();
+
+            setFacilityDetais(data);
+
+        } catch(error) {
+            if (error instanceof TypeError) {
+                console.log("Erro de rede ou falha de conexão: ", error.message);
+              } else if (error instanceof SyntaxError) {
+                console.log("Erro ao processar dados JSON: ", error.message);
+              } else {
+                console.log("Erro inesperado: ", error.message);
+              }
+        }
+    }
+
+    useEffect(() => {
+        fetchFacitilyDetails();
+    }, []);
+
 
     return (
         <>
         <ToolBar />
-
+        
         <Container sx={{ paddingTop: '7rem', paddingBottom: '3rem' }}>
             <Box className="space-y-6" sx={{paddingX: isMediumScreen ? 4 : 15 }}>
                 <Typography
                     variant={isSmallScreen ? 'h4' : 'h3'}
                     sx={{ textAlign: 'center', fontWeight: 'bold', color: '#424242' }}
                 >
-                    Unidade Básica de Saúde da Vila Gomes
+                    {facilityDetails.name}
                 </Typography>
 
             <Grid2 container spacing={2} justifyContent="center">
                 <Grid2 size = {{xs: 12, sm: 4, md: 6}}>
                     {/* Map component */}
                     <Box sx={{ marginTop: '1rem' }}>
-                        <MapComponent />
+                        {facilityDetails.latitude && facilityDetails.longitude ? (
+                            <MapComponent 
+                                latitude={facilityDetails.latitude}
+                                longitude={facilityDetails.longitude}
+                            />
+                            ) : (
+                            <Typography>Loading map...</Typography>
+                        )}
                     </Box>
                 </Grid2>
 
@@ -44,29 +85,16 @@ const ListaDeUnidades: React.FC = () => {
                 <InfoCard
                     icon={<PhoneIcon fontSize="large" sx={{ color: '#4285f4' }} />}
                     title="Contato"
-                    details="+55 (11) 98765-4321"
-                />
-
-                {/* Rating */}
-                <InfoCard
-                    icon={<StarIcon fontSize="large" sx={{ color: '#4285f4' }} />}
-                    title="4.6 Estrelas"
-                    details="Avaliação do Google"
+                    details={facilityDetails.telephone}
                 />
 
                 {/* Operating Hours */}
                 <InfoCard
                     icon={<AccessTimeIcon fontSize="large" sx={{ color: '#4285f4' }} />}
                     title="Horário de Funcionamento"
-                    details="Seg-Sab: 8:00 - 18:00"
+                    details={facilityDetails.shift}
                 />
 
-                {/* Staff Info */}
-                <InfoCard
-                    icon={<PeopleIcon fontSize="large" sx={{ color: '#4285f4' }} />}
-                    title="Corpo Médico"
-                    details="10 Médicos, 5 Enfermeiros"
-                />
                 </Grid2>
             </Grid2>
 
